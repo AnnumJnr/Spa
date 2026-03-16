0d007905b51a3abc8a8f44f7420557d29e303425
Godfred Annum Jnr <gannumjnr@gmail.com>
2026-03-16 13:58:47 +0000

    Last set of changes for first phase of deployment

--- config/settings/production.py
+++ config/settings/production.py
@@ -5,17 +5,10 @@ from .base import *
 
 DEBUG = False
 
-# PostgreSQL Database
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST', 'localhost'),
-        'PORT': os.getenv('DB_PORT', '5432'),
-    }
-}
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
+
+MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Redis channel layer
 CHANNEL_LAYERS = {
